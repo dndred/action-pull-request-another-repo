821c7f6de26afe6d81c81eb11dfad6b3a66daa81
Andrey Danilov <andrey.danilov@ivelum.com>
2023-05-17 16:52:02 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,9 +18,17 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 
 echo "Cloning destination git repository"
-git clone --depth 1  --branch $INPUT_DESTINATION_HEAD_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+{
+  git clone --depth 1  --branch $INPUT_DESTINATION_HEAD_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+  cd "$CLONE_DIR"
+}
+|| {
+  git clone --depth 1  "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR" &&
+  cd "$CLONE_DIR" &&
+  git checkout -b $INPUT_DESTINATION_HEAD_BRANCH
+}
 
-cd "$CLONE_DIR"
+
 
 echo "Running command $INPUT_COMMAND"
 eval "$INPUT_COMMAND"
